19983335293cf0caa3d38361fbad0c2f4dc07560
Killer-ProEZ <ht01646@gmail.com>
2021-10-08 16:25:41 +0700

    v0.2

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Cv</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/a2c5b72efa.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <div class="container-fluid fame">
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="text-center">
+                    <img class="img-fluid avatar" src="image/Avatar.jpg" alt="">
+                    <php>
+                        <h3>Tran Phuoc Hao</h3>
+                    </php>
+                    <h5>Developer</h5>
+                </div>
+                <div class="skill">
+                    <div class="content">
+                        <h5>Introduce myseft</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias maiores odit corrupti mollitia, dicta, voluptas eos laborum repellat quos dignissimos nihil aut quam? Maiores rerum inventore dicta accusantium culpa. Eum.</p>
+                    </div>
+                    <div class="content">
+                        <h5>My skill</h5>
+                        <ul>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-cuttlefish icon"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-cuttlefish icon">#</i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-python"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-html5"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-css3-alt"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-js"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-java"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="row">
+                                    <div class="col-sm-1"><i class="fab fa-php"></i></div>
+                                    <div class="col-sm-11">
+                                        <div class="progress">
+                                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="content">
+                        <h5>Contact</h5>
+                        <br><i class="fas fa-phone icon"></i><b> Phone number:</b> 63452348</p>
+                        <p><i class="fas fa-envelope icon"></i><b>Email:</b> Hao@hhhhh</p>
+                        <p><i class="fab fa-github-alt icon"></i><b>Github:</b> <a href="https://github.com/Killer-ProEZ">Link github</a></p>
+                        <p><i class="fab fa-linkedin icon"></i><b>Linked-in:</b> <a href="https://www.linkedin.com/in/hao-tran-b7386b215/">Link Linked-in</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-7">
+                <div class="suphead content">
+                    <h5><i class="fas fa-suitcase icon"></i>Experiences</h5>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+                <div class="supcontent">
+                    <h6>Lorem ipsum, dolor</h6>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+                <div class="suphead content">
+                    <h5><i class="far fa-lightbulb icon"></i>Academic level</h5>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+                <div class="supcontent">
+                    <h6>Lorem ipsum, dolor</h6>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+                <div class="suphead content">
+                    <h5><i class="fas fa-list-ul icon"></i>Experiences</h5>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+                <div class="supcontent">
+                    <h6>Lorem ipsum, dolor</h6>
+                </div>
+                <div class="supcontent">
+                    <ul>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</body>
+
+</html>
